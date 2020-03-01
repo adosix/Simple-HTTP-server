c6867340becfb64109db4bed8ff6e64927d52989
Andrej Jezik <andrejjezik@gmail.com>
2020-03-01 21:55:41 +0100

    started processing data

--- server.py
+++ server.py
@@ -6,28 +6,39 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # associate the socket with a specific network interface
 host = '127.0.0.1'
-port = 10000    # range of possible ports   1-65535
+port = 10007    # range of possible ports   1-65535
 sock.bind((host, port))
 
 sock.listen(1)
 
 connections = []
 
-def handler(c,a):
-    global connections
+def parse(data):
+    data = data.split(" ")
+    if len(data) == 1:
+        if data == "POST" or data == "GET":
+            exit(0);
+        
+    print(len(data))
+    print(data[0])
+    print(data[1])
+
+def handler(con,a):
     while True:
-        data = c.recv(2048)
-        for connection in connections:
-            connection.send(bytes(data))
+        data = con.recv(2048)
         if not data:
-            connections.remove(c)
-            c.close()
+            con.close()
             break
+        data = str(data, 'utf-8')   #data to string
+
+        parse(data);
+
+        data = str.encode(data)     #data to bytes
+        con.send(data)
+
 while True:
-    c, a = sock.accept()
-    cThread = threading.Thread(target=handler, args=(c,a))
-    cThread.deamon = True
-    cThread.start()
-    connections.append(c)
-    print(connections)
+    con, a = sock.accept()
+    con_thread = threading.Thread(target=handler, args=(con,a))
+    con_thread.deamon = True
+    con_thread.start()
 
