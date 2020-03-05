a1dd333f0a47ced86dd0c5896757c755430b7de6
Andrej Jezik <andrejjezik@gmail.com>
2020-03-05 23:26:34 +0100

    recognizing A/PTR

--- client.py
+++ client.py
@@ -2,8 +2,8 @@
 
 import socket
 
-HOST = socket.gethostname()  # The server's hostname or IP address
-PORT = 10015        # The port used by the server
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 10016        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -12,7 +12,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print(repr(data))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b'GET /resolve?name=apple.com&type=A HTTP/1.1')
-    data = s.recv(2048)
-    print(repr(data))
+     s.connect((HOST, PORT))
+     s.sendall(b'GET /resolve?name=apple.com&type=A HTTP/1.1')
+     data = s.recv(2048)
+     print(repr(data))
--- server.py
+++ server.py
@@ -11,8 +11,8 @@ sel = selectors.DefaultSelector()
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 # associate the socket with a specific network interface
-host = socket.gethostname()
-port = 10015    # range of possible ports   1-65535
+host = '127.0.0.1'
+port = 10016    # range of possible ports   1-65535
 
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((host, port))
@@ -25,44 +25,66 @@ def error(error_c, con):
         405: "Method Not Allowed"
     }
     error_msg = codes.get(error_c, "error")
-    con.sendall(b'HTTP/1.1 '+ error_c + ' '+ error_msg + '\r\n\r\n')
     print("ERROR: " + error_msg)
+
+    message = str.encode('HTTP/1.1 '+ str(error_c) + ' '+ error_msg + '\r\n\r\n')
+    con.sendall(message)
+    con.shutdown(1)
     exit(error_c)
+def ok(con,msg):
+    message = str.encode('HTTP/1.1 200 OK\r\n\r\n' + msg)
+    con.sendall(message)
+    con.close()
+    exit(0)
 
 def get_req(row, con):
-    #pattern = re.compile(r"\b" + "\/resolve?name=" + r"\b")
-    #print(row[1])
     match = re.search(r'^\/resolve\?name=', row[1])
     if match:
         url = row[1].split("/resolve?name=",1)[1] 
         url = url.split("&type=",1)[0] 
+        
         url_type = row[1].split("&type=",1)[1] 
+        if url_type == "A":
+            pattern = r"^(?:(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])(\.(?!$)|$)){4}$"
+            match = re.match(pattern, url)
+            if match is None:
+                error(400, con)
+                
+            url = socket.gethostbyname(url)
+            print(url)
+            print(url_type)
+            ok(con, url)
 
-        print(url)
-        print(url_type)
-        # from ip to dn GetnameInfo()
-        #row = str.encode(','.join(row))     #data to bytes
-        url = str.encode(url_type)
-        con.send(url)
+        elif url_type == "PTR":
+            pattern = r"^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$"
+            match = re.match(pattern, url)
+            if match is None:
+                error(400, con)
+            print(url)
+            print(url_type)
+            ok(con, url)
+
     else:
         error(400, con)
     exit(0)
 
 def parse(data,con):
+    data = data.split("HTTP/1.1",1)[0] + "HTTP/1.1"
     data = data.split('\n\r')
     for row in data: 
         row = row.split(' ')
+        print(row)
         if len(row) != 3:
             if row[0] == "POST" or row[0] == "GET":
-                print(len(data))
+                print(len(row))
+                print(row)
                 error(400, con)
             else:
-                con.close()
                 error(405, con)
         else:
             if row[0] == "POST" or row[0] == "GET":
                 if row[2] != "HTTP/1.1" or len(data) != 1 and row[0] == "GET":
-                    error(400)
+                    error(400,con)
                 elif row[0] == "GET":
                     get_req(row, con)
             else:
@@ -76,9 +98,8 @@ def handler(con,a):
             con.close()
             break
         data = str(data, 'utf-8')   #data to string
-
         parse(data,con)
-        con.shutdown()
+        con.close()
 
 try:
     while True:
